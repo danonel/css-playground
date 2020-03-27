576f0c049dbe953c5903908ba24759dd4fb09387
daniel <daniel>
2020-03-27 00:15:37 -0300

    adicionado checkmark para os botões

--- main.js
+++ main.js
@@ -1,29 +1,55 @@
+console.log('pra cima deles hadbala')
 
 
 function justifyContent(eventTarget){
     var boxes1 = document.querySelector('.boxes1')
     boxes1.style.justifyContent= eventTarget.value
-    var justifyContent = document.querySelector(".code-justify-result")
-    
-    justifyContent.innerHTML = `${eventTarget.value};`
 
-}
+    var justifyContent = document.querySelector(".code-justify-result")
+    var checkConfirm = document.querySelectorAll('#justifyContent>input')
+
+      
+    checkMark(checkConfirm, eventTarget)
+    justifyContent.innerHTML = ` ${eventTarget.value};` 
+
+} 
 
 function alignItems(eventTarget){
     var boxes1 = document.querySelector('.boxes1')
     boxes1.style.alignItems= eventTarget.value
     var alignItems = document.querySelector('.code-align-result')
+    
+    var checkConfirm = document.querySelectorAll('#alignItems>input')
 
+    checkMark(checkConfirm, eventTarget)
     alignItems.innerHTML = `${eventTarget.value};`
 }
 
 function flexDirection(eventTarget){
     var boxes1 = document.querySelector('.boxes1')
-    boxes1.style.flexDirection = eventTarget.value
-    
+    boxes1.style.flexDirection = eventTarget.value    
+
     var flexDirection = document.querySelector('.code-direction-result')
+    
+    var checkConfirm = document.querySelectorAll('#flexDirection>input')
+    checkMark(checkConfirm, eventTarget)
     flexDirection.innerHTML = `${eventTarget.value};`
 
 }
 
-console.log("pra cima deles hadbala")
+
+function checkMark(checkConfirm, eventTarget){
+    checkConfirm.forEach((button)=>{
+
+        eventTarget.style.background = '#BF1E2C'
+
+        var checkedButton = eventTarget.style.background
+
+        
+       if(checkedButton && button.style.background){
+           
+           button.style.background = '#BF1E2C'
+       }
+       button.style.background ='#282a36'
+    }) 
+}
