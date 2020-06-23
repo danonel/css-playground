83e0cef33f4e066011380230b1a3a82fa92e51ca
Filipez <filipezmarron@hotmail.com>
2020-06-22 21:51:45 -0300

    Restructuring files

--- js/flex.js
+++ js/flex.js
@@ -40,7 +40,6 @@ function flexDirection(eventTarget){
 }
 
 
-
 function checkMark(checkConfirm, eventTarget ){
   
   
@@ -60,12 +59,18 @@ function toggleNav(x) {
     x.classList.toggle("change");
 
 if(navIsOpen){
-  document.querySelector(".sidebar-button").style.left = "5px";  document.getElementById("mySidenav").style.left = "-250px";
+  document.querySelector(".sidebar-button").style.left = "15px";  
+  document.querySelector(".sidebar-button").style.transitionDelay = "0s";  
+  document.getElementById("mySidenav").style.left = "-250px";
+  
+  
   navIsOpen = false
   return
 }
- document.querySelector(".sidebar-button").style.left = "205px"; document.getElementById("mySidenav").style.left = "0";
+ document.querySelector(".sidebar-button").style.left = "205px"; 
+ document.getElementById("mySidenav").style.left = "0";
   navIsOpen = true
   return
 }
+
 
