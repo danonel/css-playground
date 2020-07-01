bf27219c85b1687a6625104c88c75f5a0b305e52
Daniel Luis <56178612+danonel@users.noreply.github.com>
2020-07-01 04:11:50 -0300

    Add: toggle flex wrap, box controlers and align-content into DOM, Add flexbox descriptions into DOM

--- js/flex.js
+++ js/flex.js
@@ -1,13 +1,13 @@
-console.log('pra cima deles hadbala')
+console.log('opa')
+console.log('Made by Daniel and FilipeZ')
 
 
-
+const boxes1 = document.querySelector('.boxes1')
 function justifyContent(eventTarget){
-    var boxes1 = document.querySelector('.boxes1')
     boxes1.style.justifyContent= eventTarget.value
 
-    var justifyContent = document.querySelector(".code-justify-result")
-    var checkConfirm = document.querySelectorAll('#justifyContent>input')
+    const justifyContent = document.querySelector(".code-justify-result")
+    const checkConfirm = document.querySelectorAll('#justifyContent>input')
 
     
     checkMark(checkConfirm, eventTarget)
@@ -17,33 +17,41 @@ function justifyContent(eventTarget){
 } 
 
 function alignItems(eventTarget){
-    var boxes1 = document.querySelector('.boxes1')
-    boxes1.style.alignItems= eventTarget.value
-    var alignItems = document.querySelector('.code-align-result')
     
-    var checkConfirm = document.querySelectorAll('#alignItems>input')
+    boxes1.style.alignItems= eventTarget.value
+    const alignItems = document.querySelector('.code-align-result')
+    
+    const checkConfirm = document.querySelectorAll('#alignItems>input')
 
     checkMark(checkConfirm, eventTarget)
     alignItems.innerHTML = `${eventTarget.value};`
 }
 
 function flexDirection(eventTarget){
-    var boxes1 = document.querySelector('.boxes1')
+    
     boxes1.style.flexDirection = eventTarget.value    
 
-    var flexDirection = document.querySelector('.code-direction-result')
+    const flexDirection = document.querySelector('.code-direction-result')
+    const checkConfirm = document.querySelectorAll('#flexDirection>input')
     
-    var checkConfirm = document.querySelectorAll('#flexDirection>input')
     checkMark(checkConfirm, eventTarget)
     flexDirection.innerHTML = `${eventTarget.value};`
 
 }
 
+function alignContent(eventTarget){
+  
+  boxes1.style.alignContent = eventTarget.value
+
+  const alignContent = document.querySelector('.code-align-content-result')
+  const checkConfirm = document.querySelectorAll('#alignContent>input')
+  
+  checkMark(checkConfirm, eventTarget)
+  alignContent.innerHTML = `${eventTarget.value}`
+}
 
 function checkMark(checkConfirm, eventTarget ){
-  
-  
-  var buttonArray = [...checkConfirm]
+  const buttonArray = [...checkConfirm]
   
   buttonArray.map(button=>{
     if(eventTarget!==button){
@@ -51,8 +59,42 @@ function checkMark(checkConfirm, eventTarget ){
        }else{
          button.style.background = '#BF1E2C'
        }
-    
   })
 }
 
+const checkbox = document.querySelector('#flexWrap')
+
+  checkbox.addEventListener('click', (e)=>{
+  
+  const isChecked = checkbox.checked
+  const flexWrap = document.querySelector('.code-flexWrap-result')
+
+  isChecked ? flexWrap.innerHTML = 'wrap' : flexWrap.innerHTML = 'nowrap'
+  if(isChecked){
+    boxes1.style.flexWrap = 'wrap'
+   
+    return
+  }
+  boxes1.style.flexWrap = 'nowrap'
+  return
+})
+
+
+document.querySelector('input[type=range]').addEventListener('input', (e)=>{
+  const inputRangeValue = e.target.value
+  document.querySelector('#numberOfBoxes').innerHTML = inputRangeValue
+  console.log(inputRangeValue)
+
+  function makeDiv(number){
+  let divs = []
+   
+       for (i = 0; i<number; i++){
+        divs.push(  `<div></div>`)
+       }
+     document.querySelector('.boxes1').innerHTML = divs.map(div => `${div}`).join('')
+    
+
+  }
+  makeDiv(e.target.value)
+})
 
